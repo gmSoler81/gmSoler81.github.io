20e95ea53087dc511dbc655afffb7af244f57768
gmSoler81 <121064789+gmSoler81@users.noreply.github.com>
2024-05-25 21:31:13 -0300

    Add files via upload

--- enviarformulario.php
+++ enviarformulario.php
@@ -1,22 +1,52 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/PHPMailer/src/Exception.php';
+require 'vendor/phpmailer/PHPMailer/src/PHPMailer.php';
+require 'vendor/phpmailer/PHPMailer/src/SMTP.php';
+
+require 'vendor/autoload.php';
+
+header('Content-Type: application/json');
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $mensaje = $_POST['mensaje'];
-    
-    $to = "gmsoler81@gmail.com"; // Coloca aquí tu dirección de correo electrónico
-    $subject = "Nuevo mensaje de contacto desde tu sitio web";
-    $message = "Nombre: " . $nombre . "\n";
-    $message .= "Email: " . $email . "\n";
-    $message .= "Mensaje:\n" . $mensaje . "\n";
-    
-    $headers = "From: " . $email . "\r\n";
-    $headers .= "Reply-To: " . $email . "\r\n";
-    
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Tu mensaje ha sido enviado correctamente. ¡Gracias por ponerte en contacto!";
-    } else {
-        echo "Lo sentimos, ha ocurrido un error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.";
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'gmsoler81@gmail.com';
+        $mail->Password = 'koagphqfzsisqxrg';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->setFrom('gmsoler81@gmail.com', 'Gustavo');
+        $mail->addAddress('gmsoler81@gmail.com');
+
+        $cuerpo = 'Este es un mensaje de: ' . $nombre . "\n";
+        $cuerpo .= 'Su email es: ' . $email . "\n";
+        $cuerpo .= 'Su mensaje es: ' . $mensaje;
+
+        $mail->isHTML(false);
+        $mail->Subject = 'Consulta de clases';
+        $mail->Body = $cuerpo;
+
+        $mail->send();
+
+        echo json_encode(['status' => 'success', 'message' => '¡Gracias por tu mensaje!']);
+    } catch (Exception $e) {
+        echo json_encode(['status' => 'error', 'message' => 'Error al enviar el correo: ' . $mail->ErrorInfo]);
     }
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Método de solicitud no permitido']);
 }
 ?>
